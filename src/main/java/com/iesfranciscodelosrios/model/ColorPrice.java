8214dc156510e2fd976b6385f2732da75ee8de17
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2022-01-31 08:52:52 +0100

    jamm-01
    -Precio color
    -Precio grosor paginas

--- src/main/java/com/iesfranciscodelosrios/model/ColorPrice.java
+++ src/main/java/com/iesfranciscodelosrios/model/ColorPrice.java
@@ -1,5 +1,80 @@
 package com.iesfranciscodelosrios.model;
 
-public class ColorPrice {
 
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="ColorPrice")
+public class ColorPrice implements Serializable{
+	
+	private static final long serialVersionUID=1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="price")
+	private Float price;
+	
+	@Column(name="description")
+	private String description;
+	
+	@Column(name="valid")
+	private boolean valid;
+
+	public ColorPrice(Float price, String description, boolean valid) {
+		super();
+		this.id = -1L;
+		this.price = price;
+		this.description = description;
+		this.valid = valid;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	@Override
+	public String toString() {
+		return "ColorPrice [id=" + id + ", price=" + price + ", description=" + description + ", valid=" + valid + "]";
+	}
+	
+	
 }
