6b415d83753fe5500609549f2b4da7c18cbca158
Álvaro Pérez Urbano <alvaro_beta@hotmail.com>
2022-01-31 18:32:16 +0100

    Clases Order y Discount creadas

--- src/main/java/com/iesfranciscodelosrios/model/Order.java
+++ src/main/java/com/iesfranciscodelosrios/model/Order.java
@@ -1,5 +1,90 @@
 package com.iesfranciscodelosrios.model;
 
-public class Order {
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Order implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private LocalDateTime pickupDate;
+    private User user;
+    private boolean isPayed;
+    private boolean isPickedUp;
+    private double finalPrice;
+    private List<Discount> discounts;
+
+    public Order(){
+        this.id = -1L;
+    }
+
+    public Order(Long id, LocalDateTime pickupDate, User user, boolean isPayed, boolean isPickedUp, double finalPrice, List<Discount> discounts) {
+        this.id = id;
+        this.pickupDate = pickupDate;
+        this.user = user;
+        this.isPayed = isPayed;
+        this.isPickedUp = isPickedUp;
+        this.finalPrice = finalPrice;
+        this.discounts = discounts;
+    }
+
+    public Order(Long id, LocalDateTime pickupDate, User user, boolean isPayed, boolean isPickedUp, List<Discount> discounts) {
+        this.id = id;
+        this.pickupDate = pickupDate;
+        this.user = user;
+        this.isPayed = isPayed;
+        this.isPickedUp = isPickedUp;
+        this.discounts = discounts;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getPickupDate() {
+        return pickupDate;
+    }
+
+    public void setPickupDate(LocalDateTime pickupDate) {
+        this.pickupDate = pickupDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isPayed() {
+        return isPayed;
+    }
+
+    public void setPayed(boolean payed) {
+        isPayed = payed;
+    }
+
+    public boolean isPickedUp() {
+        return isPickedUp;
+    }
+
+    public void setPickedUp(boolean pickedUp) {
+        isPickedUp = pickedUp;
+    }
+
+    public double getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
 
 }
