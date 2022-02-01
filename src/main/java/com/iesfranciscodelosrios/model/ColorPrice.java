b50fc0c461652dce2f153ad2d7386dd50be7d801
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2022-02-01 08:44:05 +0100

    subida 2
    adición de el método "equals", constructor y otros campos

--- src/main/java/com/iesfranciscodelosrios/model/ColorPrice.java
+++ src/main/java/com/iesfranciscodelosrios/model/ColorPrice.java
@@ -2,6 +2,7 @@ package com.iesfranciscodelosrios.model;
 
 
 
+import java.io.Serial;
 import java.io.Serializable;
 
 import javax.persistence.Column;
@@ -15,6 +16,7 @@ import javax.persistence.Table;
 @Table(name="ColorPrice")
 public class ColorPrice implements Serializable{
 	
+	@Serial
 	private static final long serialVersionUID=1L;
 	
 	@Id
@@ -38,6 +40,15 @@ public class ColorPrice implements Serializable{
 		this.description = description;
 		this.valid = valid;
 	}
+
+	
+	
+	public ColorPrice() {
+		super();
+		this.id=-1L;
+	}
+
+
 
 	public Long getId() {
 		return id;
@@ -75,6 +86,37 @@ public class ColorPrice implements Serializable{
 	public String toString() {
 		return "ColorPrice [id=" + id + ", price=" + price + ", description=" + description + ", valid=" + valid + "]";
 	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ColorPrice other = (ColorPrice) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }
