10f4e3288cfcb5d1a6cc8bcb4c795f8290532c2c
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2022-02-03 17:43:11 +0100

    service user

--- src/main/java/com/iesfranciscodelosrios/services/UserService.java
+++ src/main/java/com/iesfranciscodelosrios/services/UserService.java
@@ -1,6 +1,11 @@
 package com.iesfranciscodelosrios.services;
 
+import com.iesfranciscodelosrios.model.User;
 import com.iesfranciscodelosrios.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +13,40 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     UserRepository userRepository;
-
+    public List<User> getAllUsers(){
+		List<User> result=userRepository.findAll();//cambiar null por el repositorio
+		return result;
+	}
+	
+	public User createOrUpdateUser(User user)throws Exception{
+		if(user.getId()!=null && user.getId()>0) {
+			Optional<User> n=  userRepository.findById(user.getId());
+			if(n.isPresent()) {
+				User newUser=n.get();
+				newUser.getId();
+				newUser.getMail();
+				newUser.getName();
+				newUser.getPhoneNumber();
+				newUser.getUserOrders();
+				newUser=userRepository.save(newUser);
+				return newUser;
+			}else {
+				user.setId(null);
+				user=userRepository.save(user);
+				return user;
+			}
+		}else {
+			user=userRepository.save(user);
+			return user;
+		}
+	}
+	
+	public void deleteUserById(Long id)throws Exception	{
+		Optional<User> user=userRepository.findById(id);
+		if(user.isPresent()) {
+			userRepository.deleteById(id);
+		}else {
+			throw  new Exception() ;
+		}
+	}
 }
