f8d96504cbfcae619c0f716b3202fb77732d3597
joseangelmarinmoreno <60347715+josang1567@users.noreply.github.com>
2022-02-04 09:07:28 +0100

    User services 4
    -userService: find by mail
    
    -user repository: query find by mail

--- src/main/java/com/iesfranciscodelosrios/repositories/UserRepository.java
+++ src/main/java/com/iesfranciscodelosrios/repositories/UserRepository.java
@@ -1,8 +1,18 @@
 package com.iesfranciscodelosrios.repositories;
 
 import com.iesfranciscodelosrios.model.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
+	
+	//buscar usuario segun el correo
+	@Query(nativeQuery= false, value= "SELECT u FROM User u WHERE mail= ?1")
+	Optional<User> findByMail(String mail);
+	//
+	
 }
