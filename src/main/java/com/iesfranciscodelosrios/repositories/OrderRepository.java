767189851c229a108a0a9db2636ad6e9c1b0f6d8
R3ktEr <ElRepu@outlook.es>
2022-02-07 08:57:40 +0100

    Terminados los servicios de Document y Order

--- src/main/java/com/iesfranciscodelosrios/repositories/OrderRepository.java
+++ src/main/java/com/iesfranciscodelosrios/repositories/OrderRepository.java
@@ -1,8 +1,14 @@
 package com.iesfranciscodelosrios.repositories;
 
 import com.iesfranciscodelosrios.model.Order;
+import com.iesfranciscodelosrios.model.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+	@Query("Select o from Order o where o.user = ?1")
+	List<Order> getOrdersByUser(User u);
 }
