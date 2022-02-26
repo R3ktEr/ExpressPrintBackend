69c34566fb7181a0b3e52cee0e8093f056e89c47
R3ktEr <ElRepu@outlook.es>
2022-02-26 11:46:52 +0100

    Heroku2.0

---
+++ src/main/java/com/iesfranciscodelosrios/tools/Credentials.java
@@ -0,0 +1,7 @@
+package com.iesfranciscodelosrios.tools;
+
+public class Credentials {
+    public static final String USERMAIL= "backendexpressprint@gmail.com";
+    public static final String ACCESSTOKEN = "ya29.A0ARrdaM_-iIePFsUaZyJr_8aH-9mwxXy44UidxqswCQ2TX6n2YedMZ5FtGCoRyuprulUVBX3fAig4V2v4mbqd0weLsgRqGlRn0jIN0bq9JUrH8PKmWSu6l73bZsf2CBW1q-JnIEGTueHtWzgHnuUBjxEn-M_N";
+    public static final String REFRESTOKEN = "1//04IpX3dixd4UdCgYIARAAGAQSNwF-L9Ir0Z5rrYx0fYFu7QkdvaAPqr1ZvkEnvQ4CmPUivUKMRxiWu6JDdacjzWZ8kwkUbbiPMcw";
+}
